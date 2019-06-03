4d158bcd7329265df8baa47a4c09d514a8cc158a
YannickGHS <39533108+YannickGHS@users.noreply.github.com>
2019-06-03 19:15:56 -0400

    Delete molecule.class.js

--- Chemistry-Summative-master/molecule.class.js
+++
@@ -1,9 +0,0 @@
-class Molecule{
-    constructor(name){
-        this.name = name;
-        this.formula;
-        this.type = 'undefined'; //type can be organic, big, small
-        this.forces = 'none'; //intermolecular forces
-    }
-    
-}
